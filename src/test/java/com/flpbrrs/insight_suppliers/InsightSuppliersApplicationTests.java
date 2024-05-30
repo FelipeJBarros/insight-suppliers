7a78ee467ac4c09f42c92c62c37aa375c38d41e7
Felipe Barros <felipebarros.engh@gmail.com>
2024-05-30 16:10:49 -0300

    feat: initial files

---
+++ src/test/java/com/flpbrrs/insight_suppliers/InsightSuppliersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flpbrrs.insight_suppliers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsightSuppliersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
