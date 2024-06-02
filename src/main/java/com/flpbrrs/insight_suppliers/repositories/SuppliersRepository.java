78e7f2412edb966c07faa65f8aa1cbdaeab3c518
Felipe Barros <felipebarros.engh@gmail.com>
2024-06-02 07:32:13 -0300

    feat: creating supplier repository

---
+++ src/main/java/com/flpbrrs/insight_suppliers/repositories/SuppliersRepository.java
@@ -0,0 +1,11 @@
+package com.flpbrrs.insight_suppliers.repositories;
+
+import com.flpbrrs.insight_suppliers.models.Supplier;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface SuppliersRepository extends JpaRepository<Supplier, UUID> {
+}
