df6b2513e3e3f7e5553367c487eef64bc1d29047
Felipe Barros <felipebarros.engh@gmail.com>
2024-06-06 19:39:02 -0300

    feat: adding business rules on suppliers services

--- src/main/java/com/flpbrrs/insight_suppliers/repositories/SuppliersRepository.java
+++ src/main/java/com/flpbrrs/insight_suppliers/repositories/SuppliersRepository.java
@@ -5,8 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface SuppliersRepository extends JpaRepository<Supplier, UUID>, JpaSpecificationExecutor<Supplier> {
+    boolean existsByEmail(String email);
+    boolean existsByDocNumber(String docNumber);
+    Optional<Supplier> findByEmail(String email);
+    Optional<Supplier> findByDocNumber(String docNumber);
 }
--- src/main/java/com/flpbrrs/insight_suppliers/services/SuppliersServices.java
+++ src/main/java/com/flpbrrs/insight_suppliers/services/SuppliersServices.java
@@ -3,6 +3,7 @@ package com.flpbrrs.insight_suppliers.services;
 import com.flpbrrs.insight_suppliers.dtos.PageDTO;
 import com.flpbrrs.insight_suppliers.dtos.SupplierDTO;
 import com.flpbrrs.insight_suppliers.dtos.SupplierOption;
+import com.flpbrrs.insight_suppliers.exceptions.DomainException;
 import com.flpbrrs.insight_suppliers.models.Supplier;
 import com.flpbrrs.insight_suppliers.models.specifications.SupplierSpec;
 import com.flpbrrs.insight_suppliers.repositories.SuppliersRepository;
@@ -14,6 +15,7 @@ import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -50,13 +52,28 @@ public class SuppliersServices {
     }
 
     public SupplierDTO create(SupplierDTO dto) {
+        if(suppliersRepository.existsByEmail(dto.getEmail()))
+            throw new DomainException("Este e-mail já está em uso");
+
+        if(suppliersRepository.existsByDocNumber(dto.getDocNumber()))
+            throw new DomainException("Número de documento já em uso");
+
         Supplier supplier = suppliersRepository.save(modelMapper.map(dto, Supplier.class));
         return modelMapper.map(supplier, SupplierDTO.class);
     }
 
     public SupplierDTO update(UUID supplierId, SupplierDTO dto) {
-        boolean supplierExists = suppliersRepository.findById(supplierId).isPresent();
-        if(!supplierExists) throw new RuntimeException("Supplier don't exists");
+        if(!suppliersRepository.existsById(dto.getId()))
+            throw new DomainException("Fornecedor com este id não existe");
+
+        Optional<Supplier> supplierByEmail = suppliersRepository.findByEmail(dto.getEmail());
+        if (supplierByEmail.isPresent() && !supplierByEmail.get().getId().equals(dto.getId()))
+            throw new DomainException("Este e-mail já está em uso");
+
+        Optional<Supplier> supplierByDocNumber = suppliersRepository.findByDocNumber(dto.getDocNumber());
+        if (supplierByDocNumber.isPresent() && !supplierByDocNumber.get().getId().equals(dto.getId()))
+            throw new DomainException("Número de documento já em uso");
+
         dto.setId(supplierId);
         Supplier supplier = suppliersRepository.save(modelMapper.map(dto, Supplier.class));
         return modelMapper.map(supplier, SupplierDTO.class);
